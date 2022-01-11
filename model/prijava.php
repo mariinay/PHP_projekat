0a0c2633194e422371b3b1e68b9aad9b4585c12c
mariinay <marina.jovanovic.mj@gmail.com>
2022-01-11 22:39:01 +0100

    Izmenjen model Prijava

--- model/prijava.php
+++ model/prijava.php
@@ -1,17 +1,15 @@
 <?php
 class Prijava{
     public $id;   
-    public $ime;
-    public $prezime;   
+    public $teretana;  
     public $lokacija; 
     public $datum;     
     public $vreme;
     
-    public function __construct($id=null, $ime=null, $prezime=null,  $lokacija=null, $datum=null, $vreme=null)
+    public function __construct($id=null, $teretana=null, $lokacija=null, $datum=null, $vreme=null)
     {
         $this->id = $id;
-        $this->ime = $ime;
-        $this->prezime = $prezime;
+        $this->teretana = $teretana;
         $this->lokacija = $lokacija;
         $this->datum = $datum;
         $this->vreme = $vreme;
@@ -52,14 +50,14 @@ class Prijava{
 
     public function update($id, mysqli $conn)
     {
-        $query = "UPDATE prijave set ime = $this->ime, prezime = $this->prezime, lokacija = $this->lokacija, datum = $this->datum, vreme = $this->vreme WHERE id=$id";
+        $query = "UPDATE prijave set teretana = $this->teretana, lokacija = $this->lokacija, datum = $this->datum, vreme = $this->vreme WHERE id=$id";
         return $conn->query($query);
     }
 
 
     public static function add(Prijava $prijava, mysqli $conn)
     {
-        $query = "INSERT INTO prijave(ime, prezime, lokacija, datum, vreme) VALUES('$prijava->ime', '$prijava->prezime', '$prijava->lokacija','$prijava->datum', '$prijava->vreme')";
+        $query = "INSERT INTO prijave(teretana, lokacija, datum, vreme) VALUES('$prijava->teretana', '$prijava->lokacija','$prijava->datum', '$prijava->vreme')";
         return $conn->query($query);
     }
 }
