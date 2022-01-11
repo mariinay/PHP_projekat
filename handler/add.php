320fe9cc7deb8f270dd705c8e67b199e89b8dc53
mariinay <marina.jovanovic.mj@gmail.com>
2022-01-11 23:13:56 +0100

    Implementirane operacije add, post i get

---
+++ handler/add.php
@@ -0,0 +1,20 @@
+<?php
+
+require "../dbBroker.php";
+require "../model/prijava.php";
+
+if(isset($_POST['teretana']) && isset($_POST['lokacija']) 
+&& isset($_POST['datum']) && isset($_POST['vreme'])){
+    $prijava = new Prijava(null,$_POST['teretana'],$_POST['lokacija'],$_POST['datum'],$_POST['vreme'] );
+    $status = Prijava::add($prijava, $conn);
+
+    if($status){
+        echo 'Success';
+    }else{
+        echo $status;
+        echo 'Failed';
+    }
+}
+
+
+?>
